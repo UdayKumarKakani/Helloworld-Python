03d7069c3a3645c0573e9f8b7e6305e4bd9a1313
Uday Kumar <38879020+UdayKumarKakani@users.noreply.github.com>
2024-06-29 10:08:21 +0530

    First commit by uday

--- launch.py
+++ launch.py
@@ -2,6 +2,6 @@ from flask import Flask
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
-    return "{\"message\":\"Data from Microservice Three - Payment\"}"
+    return "{\"message\":\"Data from Microservice Three - Payment service\"}"
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
